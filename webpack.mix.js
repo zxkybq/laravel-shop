bc40ccf7dc151d846205d5e9a57a14f68454d5ef
zxkyb <648204712@qq.com>
2018-08-23 14:28:50 +0000

    基础布局

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,11 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+/*
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+*/
+//修改为
+mix.js('resources/assets/js/app.js', 'public/js')
+	.sass('resources/assets/sass/app.scss', 'public/css')
+	.version();
