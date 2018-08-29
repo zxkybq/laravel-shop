8e56f0892c112b850b0fccf3552cfc9e6e81d497
zxkyb <648204712@qq.com>
2018-08-29 15:01:15 +0000

    用户界面 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,15 @@ Route::group([
     $router->get('/', 'HomeController@index');
     //用户管理
 	$router->get('users', 'UsersController@index');
+
+	//商品列表
+	$router->get('products', 'ProductsController@index');
+	//添加商品
+	$router->get('products/create', 'ProductsController@create');
+	//保存商品
+    $router->post('products', 'ProductsController@store');
+    //编辑页面
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    //编辑
+	$router->put('products/{id}', 'ProductsController@update');
 });
