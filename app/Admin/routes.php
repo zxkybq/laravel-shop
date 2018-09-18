f6024f103d6718a67b952205b01f8be33da5995c
zxkyb <648204712@qq.com>
2018-09-18 06:47:09 +0000

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,17 +11,25 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
-    //用户管理
+    // 用户管理
 	$router->get('users', 'UsersController@index');
 
-	//商品列表
+	// 商品列表
 	$router->get('products', 'ProductsController@index');
-	//添加商品
+	// 添加商品
 	$router->get('products/create', 'ProductsController@create');
-	//保存商品
+	// 保存商品
     $router->post('products', 'ProductsController@store');
-    //编辑页面
+    // 编辑页面
     $router->get('products/{id}/edit', 'ProductsController@edit');
-    //编辑
+    // 编辑
 	$router->put('products/{id}', 'ProductsController@update');
+
+
+    // 订单列表
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    // 订单详情
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    // 订单发货
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
